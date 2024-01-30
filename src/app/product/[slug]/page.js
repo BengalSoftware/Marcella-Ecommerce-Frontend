386fd1f68133506d6b12f5f76924e42864165874
adnanhjoy <engr.adnanhossain@gmail.com>
2024-01-30 16:07:38 +0600

    add breadcrumb

--- src/app/product/[slug]/page.js
+++ src/app/product/[slug]/page.js
@@ -4,11 +4,13 @@ import img from '../../../../public/assets/product.webp'
 import ProductDescription from '@/components/productDetails/ProductDescription';
 import ProductDeliveryInfo from '@/components/productDetails/ProductDeliveryInfo';
 import RelatedProducts from '@/components/productDetails/RelatedProducts';
+import BreadCrumb from '@/components/breadCrumb/BreadCrumb';
 
 const ProductDetailsPage = () => {
     return (
-        <div className='container mx-auto my-10'>
+        <div className='container mx-auto mb-10 mt-5'>
             <div className='mx-4 md:mx-0'>
+                <BreadCrumb />
                 <div className='grid grid-cols-1 lg:grid-cols-5'>
                     <div className='col-span-2'>
                         <Image quality={100} placeholder='blur' src={img} alt='detail' />
--- src/components/breadCrumb/BreadCrumb.jsx
+++ src/components/breadCrumb/BreadCrumb.jsx
@@ -1,9 +1,13 @@
+import Link from 'next/link';
 import React from 'react';
+import { IoHomeSharp } from "react-icons/io5";
 
 const BreadCrumb = () => {
     return (
-        <div>
-            
+        <div className='flex items-center gap-2 text-sm mb-5'>
+            <Link href='/' className='hover:text-primary text-dark'><IoHomeSharp /></Link>
+            <Link href='/' className='hover:text-primary text-dark'> / Products</Link>
+            <Link href='/' className='hover:text-primary text-dark'> / Nova Automatic Cordless Stainless</Link>
         </div>
     );
 };
