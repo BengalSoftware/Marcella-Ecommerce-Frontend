0c7f4fbb36c23de6d05068d5bbdad56c61127a6c
adnanhjoy <engr.adnanhossain@gmail.com>
2024-01-30 11:10:23 +0600

    responsive top brands

--- src/components/home/topBrands/TopBrands.jsx
+++ src/components/home/topBrands/TopBrands.jsx
@@ -4,14 +4,15 @@ import Image from 'next/image';
 
 const TopBrands = () => {
     return (
-        <div>
+        <div className='lg:mt-16'>
             <h1 className='text-center font-semibold text-3xl'>Top Brands</h1>
 
-            <div className='grid grid-cols-2 md:grid-cols-4 xl:grid-cols-6 gap-4'>
+            <div className='grid grid-cols-2 md:grid-cols-4 xl:grid-cols-6 gap-4 mt-8'>
                 {
                     Array(12).fill().map((_, idx) =>
-                        <div key={idx} className='bg-white rounded-lg shadow p-5'>
+                        <div key={idx} className='bg-white rounded-lg shadow-md cursor-pointer px-14 py-10 group'>
                             <Image quality={100} placeholder='blur' src={img} alt='brand' />
+                            <h1 className='text-center group-hover:text-primary mt-3'>Fresh</h1>
                         </div>
                     )
                 }
