1bdfee39bc43c2edc9671efa43e3c3d6c3e48b63
adnanhjoy <engr.adnanhossain@gmail.com>
2024-01-30 16:12:26 +0600

    add modified related product and product card

--- src/components/card/ProductCard.jsx
+++ src/components/card/ProductCard.jsx
@@ -1,20 +1,23 @@
 import Image from 'next/image';
 import React from 'react';
 import img from '../../../public/assets/product.webp'
+import Link from 'next/link';
 
 const ProductCard = ({ imgs }) => {
     return (
-        <div className='bg-white rounded-lg cursor-pointer relative shadow'>
-            <p className='bg-g-primary absolute text-white top-0 left-0 rounded-tl-lg rounded-br-lg text-sm px-4 py-2 z-50'>Flat 10 TK off</p>
-            <div className='overflow-hidden rounded-t-lg'>
-                <Image quality={100} placeholder='blur' className='rounded-t-lg hover:scale-110 ease-in-out duration-700' src={imgs ? imgs : img} alt="" />
+        <Link href={`/product/Nova Automatic Cordless Stainless Steel Electric Kettle 1.8 Liter Ae-1818`}>
+            <div className='bg-white rounded-lg cursor-pointer relative shadow'>
+                <p className='bg-g-primary absolute text-white top-0 left-0 rounded-tl-lg rounded-br-lg text-sm px-4 py-2 z-50'>Flat 10 TK off</p>
+                <div className='overflow-hidden rounded-t-lg'>
+                    <Image quality={100} placeholder='blur' className='rounded-t-lg hover:scale-110 ease-in-out duration-700' src={imgs ? imgs : img} alt="" />
+                </div>
+                <div className='p-2'>
+                    <p className='line-clamp-2 text-center'>Nova Automatic Cordless Stainless Steel Electric Kettle 1.8 Liter Ae-1818</p>
+                    <p className='text-center mt-2 font-medium text-primary'>BDT 680.00</p>
+                    <p className='text-center mt-2 rounded-full border w-fit mx-auto text-sm px-3 py-1 font-medium'>MRP <span className='line-through text-red-500'>1350</span></p>
+                </div>
             </div>
-            <div className='p-2'>
-                <p className='line-clamp-2 text-center'>Nova Automatic Cordless Stainless Steel Electric Kettle 1.8 Liter Ae-1818</p>
-                <p className='text-center mt-2 font-medium text-primary'>BDT 680.00</p>
-                <p className='text-center mt-2 rounded-full border w-fit mx-auto text-sm px-3 py-1 font-medium'>MRP <span className='line-through text-red-500'>1350</span></p>
-            </div>
-        </div>
+        </Link>
     );
 };
 
