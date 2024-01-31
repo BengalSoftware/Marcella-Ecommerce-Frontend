bf531326f620e1cbfea8e27cd77f1a43c051586a
adnanhjoy <engr.adnanhossain@gmail.com>
2024-01-31 15:01:07 +0600

    filter and sor for mobile device

--- src/app/products/page.js
+++ src/app/products/page.js
@@ -1,5 +1,6 @@
 import BreadCrumb from '@/components/breadCrumb/BreadCrumb';
 import FilterSidebar from '@/components/productSearching/filterSidebar/FilterSidebar';
+import MobileFilterSidebar from '@/components/productSearching/filterSidebar/MobileFilterSidebar';
 import SearchingOutlet from '@/components/productSearching/searchingOutlet/SearchingOutlet';
 import { Pagination } from 'antd';
 import React from 'react';
@@ -11,8 +12,11 @@ const ProductsPage = () => {
                 <div className="mx-4 md:mx-0">
                     <BreadCrumb />
                     <div className='md:grid grid-cols-5 gap-4'>
-                        <div className='md:col-span-1'>
+                        <div className='md:col-span-1 hidden md:block'>
                             <FilterSidebar />
+                        </div>
+                        <div className='md:hidden ml-1 md:ml-0'>
+                            <MobileFilterSidebar />
                         </div>
                         <div className='md:col-span-4'>
                             <p className='text-sm mb-5 ml-1 mt-2'>46 items found for <span className='text-primary'>"electric kettle"</span> </p>
---
+++ src/components/productSearching/filterSidebar/MobileFilterSidebar.jsx
@@ -0,0 +1,26 @@
+'use client'
+import { Drawer } from 'antd';
+import React, { useState } from 'react';
+import FilterSidebar from './FilterSidebar';
+
+const MobileFilterSidebar = () => {
+    const [open, setOpen] = useState(false);
+    return (
+        <div className='mb-4'>
+            <button onClick={() => setOpen(!open)} className='flex items-center gap-2 border rounded p-2 w-full justify-center border-primary'>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="16" height="16"><path d="M21 18V21H19V18H17V15H23V18H21ZM5 18V21H3V18H1V15H7V18H5ZM11 6V3H13V6H15V9H9V6H11ZM11 11H13V21H11V11ZM3 13V3H5V13H3ZM19 13V3H21V13H19Z"></path></svg>
+                <p className='text-gray-800 font-[300] uppercase text-sm'>Filter & Sort</p>
+            </button>
+            <Drawer
+                title="Categories"
+                placement={'left'}
+                onClose={() => setOpen(false)}
+                open={open}
+            >
+                <FilterSidebar />
+            </Drawer>
+        </div>
+    );
+};
+
+export default MobileFilterSidebar;
