d589c5a261f88f85119940226fab79f2c5ae0e53
adnanhjoy <engr.adnanhossain@gmail.com>
2024-01-31 14:39:55 +0600

    add product search page

--- src/app/products/page.js
+++ src/app/products/page.js
@@ -1,17 +1,25 @@
+import BreadCrumb from '@/components/breadCrumb/BreadCrumb';
 import FilterSidebar from '@/components/productSearching/filterSidebar/FilterSidebar';
+import SearchingOutlet from '@/components/productSearching/searchingOutlet/SearchingOutlet';
+import { Pagination } from 'antd';
 import React from 'react';
 
 const ProductsPage = () => {
     return (
         <div className="bg-[#f5f5f5]">
-            <div className="container mx-auto py-1 md:py5 xl:py-10">
+            <div className="container mx-auto py-1 md:py-5">
                 <div className="mx-4 md:mx-0">
-                    <div className='grid grid-cols-5'>
+                    <BreadCrumb />
+                    <div className='md:grid grid-cols-5 gap-4'>
                         <div className='md:col-span-1'>
                             <FilterSidebar />
                         </div>
                         <div className='md:col-span-4'>
-
+                            <p className='text-sm mb-5 ml-1 mt-2'>46 items found for <span className='text-primary'>"electric kettle"</span> </p>
+                            <SearchingOutlet />
+                            <div className='mt-5 flex items-center justify-end'>
+                                <Pagination defaultCurrent={1} total={50} />
+                            </div>
                         </div>
                     </div>
                 </div>
--- src/components/breadCrumb/BreadCrumb.jsx
+++ src/components/breadCrumb/BreadCrumb.jsx
@@ -4,7 +4,7 @@ import { IoHomeSharp } from "react-icons/io5";
 
 const BreadCrumb = () => {
     return (
-        <div className='flex items-center gap-2 text-sm mb-5'>
+        <div className='flex items-center gap-2 text-xs md:text-sm mb-5'>
             <Link href='/' className='hover:text-primary text-dark'><IoHomeSharp /></Link>
             <Link href='/' className='hover:text-primary text-dark'> / Products</Link>
             <Link href='/' className='hover:text-primary text-dark'> / Nova Automatic Cordless Stainless</Link>
