f57b809569ab20e5a80cf2c82977692479228544
adnanhjoy <engr.adnanhossain@gmail.com>
2024-02-06 16:20:29 +0600

    responsive shop page

--- src/components/shop/ShopHeader.jsx
+++ src/components/shop/ShopHeader.jsx
@@ -8,10 +8,10 @@ const ShopHeader = () => {
         <div className='relative'>
             <Image className='w-full h-36 rounded-lg' src={cover} alt='banner' quality={100} placeholder='blur' />
             <div className='bg-[#ffffffc7] absolute top-5 left-5 bottom-5 rounded-lg p-2 flex items-center gap-5'>
-                <Image className='rounded-full h-20 w-20' src={avatar} alt='avatar' quality={100} placeholder='blur' />
-                <div className='mr-20'>
-                    <h1 className='text-3xl font-medium'>Bengal Shop</h1>
-                    <p className='mt-2 text-sm'>81% Positive Seller Ratings</p>
+                <Image className='rounded-full h-16 md:h-20 w-16 md:w-20' src={avatar} alt='avatar' quality={100} placeholder='blur' />
+                <div className='mr-10 md:mr-20'>
+                    <h1 className='text-xl md:text-3xl font-medium'>Bengal Shop</h1>
+                    <p className='mt-2 text-xs md:text-sm'>81% Positive Seller Ratings</p>
                 </div>
             </div>
         </div>
--- src/components/shop/index.jsx
+++ src/components/shop/index.jsx
@@ -6,7 +6,7 @@ const SellerShopIndex = () => {
         <div className='mt-5'>
             <h1 className='md:text-2xl font-medium text-primary'>Just For you</h1>
 
-            <div className='grid grid-cols-2 md:grid-cols-3 xl:grid-cols-5 mt-5 gap-4'>
+            <div className='grid grid-cols-2 md:grid-cols-3 xl:grid-cols-5 mt-5 gap-2 md:gap-4'>
                 {
                     Array(10).fill().map((_, idx) =>
                         <ProductCard
