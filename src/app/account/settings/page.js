70648a60574c11134444452da2d8f67148b08d2c
adnanhjoy <engr.adnanhossain@gmail.com>
2024-02-06 12:09:36 +0600

    add user form

--- src/app/account/settings/page.js
+++ src/app/account/settings/page.js
@@ -1,9 +1,12 @@
+import UserSettingForm from '@/components/form/account/UserSettingForm';
 import React from 'react';
 
 const AccountSettingsPage = () => {
     return (
         <div className='bg-white shadow rounded-lg p-4'>
             <h1 className='text-xl font-medium pb-4 border-dashed border-b border-b-primary'>Settings</h1>
+
+            <UserSettingForm />
         </div>
     );
 };
---
+++ src/components/form/account/UserSettingForm.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const UserSettingForm = () => {
+    return (
+        <div className='mt-5'>
+            <form>
+                <div className='grid grid-cols-1 md:grid-cols-2 gap-5'>
+                    <div>
+                        <label className='text-dark text-sm'>Name <span className='text-red-500'>*</span></label>
+                        <input type="text" required defaultValue={'Adnan Hossain'} className='border mt-2 border-gray-300 outline-none p-2 w-full block rounded-md placeholder:text-sm placeholder:font-light' placeholder='Name' />
+                    </div>
+                    <div>
+                        <label className='text-dark text-sm'>Email <span className='text-red-500'>*</span></label>
+                        <input type="email" required defaultValue={'adnan@gmail.com'} disabled className='border mt-2 border-gray-300 outline-none p-2 w-full block rounded-md placeholder:text-sm placeholder:font-light' placeholder='Email' />
+                    </div>
+                </div>
+
+                <div className='flex items-center justify-end mt-5'>
+                    <button className='bg-primary hover:bg-dark text-white px-6 py-2 rounded-md'>Save Change</button>
+                </div>
+            </form>
+        </div>
+    );
+};
+
+export default UserSettingForm;
