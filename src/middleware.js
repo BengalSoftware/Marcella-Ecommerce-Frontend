2b618b066aa6b12ba094247c15b7fa8d31c6a0cd
adnanhjoy <engr.adnanhossain@gmail.com>
2024-02-07 14:42:32 +0600

    add middleware

--- src/middleware.js
+++ src/middleware.js
@@ -1,13 +1,40 @@
 import { NextResponse } from 'next/server';
+import { sellerData, userData } from './fakeData/userData';
+const user = userData
+const seller = sellerData
 export function middleware(request) {
-    const isSeller = false;
-    const isUser = false;
+    const response = NextResponse.next()
+    if (user?.email) {
+        response.cookies.set({
+            name: 'user',
+            value: user,
+            path: '/    '
+        })
+    } else {
+        response.cookies.delete('user');
+        console.log('user logout')
+    }
+
+    if (seller?.email) {
+        response.cookies.set({
+            name: 'seller',
+            value: seller,
+            path: '/    '
+        })
+    } else {
+        response.cookies.delete('seller');
+        console.log('seller logout')
+    }
+
+
+    const isSeller = response.cookies.get('seller');
+    const isUser = response.cookies.get('user');
 
     const currentPath = request.nextUrl.pathname;
 
-    if (isSeller && (currentPath === '/seller')) {
+    if (isSeller?.value?.email && (currentPath === '/seller')) {
         return NextResponse.redirect(new URL('/seller-signup', request.url));
-    } else if (isUser && (currentPath === '/account')) {
+    } else if (isUser?.value?.email && (currentPath === '/account')) {
         return NextResponse.redirect(new URL('/login', request.url));
     }
     return NextResponse.next();
