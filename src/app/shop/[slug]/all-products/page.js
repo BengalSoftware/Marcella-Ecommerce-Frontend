16461a7047b72725ff883b11891cd618fc30dc0b
adnanhjoy <engr.adnanhossain@gmail.com>
2024-02-11 15:53:55 +0600

    responsive all product page

--- src/app/shop/[slug]/all-products/page.js
+++ src/app/shop/[slug]/all-products/page.js
@@ -1,9 +1,29 @@
+import BreadCrumb from '@/components/breadCrumb/BreadCrumb';
+import FilterSidebar from '@/components/productSearching/filterSidebar/FilterSidebar';
+import MobileFilterSidebar from '@/components/productSearching/filterSidebar/MobileFilterSidebar';
+import SearchingOutlet from '@/components/productSearching/searchingOutlet/SearchingOutlet';
+import { Pagination } from 'antd';
 import React from 'react';
 
 const AllProductPage = () => {
     return (
         <div className='mt-5'>
-            All Products
+            <BreadCrumb />
+            <div className='md:grid grid-cols-5 gap-4'>
+                <div className='md:col-span-1 hidden md:block'>
+                    <FilterSidebar />
+                </div>
+                <div className='md:hidden ml-1 md:ml-0'>
+                    <MobileFilterSidebar />
+                </div>
+                <div className='md:col-span-4'>
+                    <p className='text-sm mb-5 ml-1 mt-2'>46 items found for <span className='text-primary'>"electric kettle"</span> </p>
+                    <SearchingOutlet />
+                    <div className='mt-5 flex items-center justify-end'>
+                        <Pagination defaultCurrent={1} total={50} />
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/shop/ShopNavbar.jsx
+++ src/components/shop/ShopNavbar.jsx
@@ -6,15 +6,15 @@ import { IoIosSearch } from 'react-icons/io';
 
 const ShopNavbar = () => {
     const pathname = usePathname();
-    
+
     return (
-        <div className='bg-white mt-4 shadow rounded-md py-3 flex items-center justify-between'>
-            <ul className='flex items-center gap-4 mx-5'>
+        <div className='bg-white mt-4 shadow rounded-md py-3 md:flex items-center justify-between'>
+            <ul className='flex items-center justify-center md:justify-start mb-5 md:mb-0 gap-4 mx-5'>
                 <li><Link href='/shop/bengal-shop' className={`py-3 border-b-2 ${(pathname === '/shop/bengal-shop') ? 'border-b-primary text-primary' : 'border-b-white'}`}>Home</Link></li>
                 <li><Link href='/shop/bengal-shop/all-products' className={`py-3 border-b-2 ${(pathname === '/shop/bengal-shop/all-products') ? 'border-b-primary text-primary' : 'border-b-white'}`}>All Products</Link></li>
                 <li><Link href='/shop/bengal-shop/profile' className={`py-3 border-b-2 ${(pathname === '/shop/bengal-shop/profile') ? 'border-b-primary text-primary' : 'border-b-white'}`}>Profile</Link></li>
             </ul>
-            <div className='flex items-center justify-center w-1/4 mx-5'>
+            <div className='flex items-center justify-center md:w-1/4 mx-5'>
                 <div className='bg-white w-full flex items-center rounded-full px-4 border border-gray-400'>
                     <input className='w-full py-2 pr-3 outline-none rounded-full placeholder:text-sm' placeholder='Search for products..' type="text" name="" id="" />
                     <IoIosSearch className='text-2xl' />
