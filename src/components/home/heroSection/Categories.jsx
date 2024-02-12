74c93923b7774f0103e83d03b1a5b31be7143cb8
adnanhjoy <engr.adnanhossain@gmail.com>
2024-02-12 16:34:00 +0600

    add category

--- src/components/home/heroSection/Categories.jsx
+++ src/components/home/heroSection/Categories.jsx
@@ -1,51 +1,46 @@
+import { useGetAllCategoryQuery } from '@/redux/categories/categoriesApi';
 import Link from 'next/link';
 import React from 'react';
 import { IoIosArrowForward } from "react-icons/io";
 
 const Categories = () => {
+    const { data: categories } = useGetAllCategoryQuery();
+    console.log(categories?.result)
     return (
         <div className='bg-white rounded-md'>
             <ul className='relative py-2'>
                 {
-                    Array(12).fill().map((_, idx) =>
-                        <li key={idx} className='group'>
-                            <Link href='/products' className='flex items-center justify-between pl-4 py-1.5 text-xs xl:text-sm text-dark hover:bg-gray-100'> <span className='line-clamp-1'>Womens & Girls Fashion</span> <IoIosArrowForward className='text-primary mr-4' /></Link>
+                    categories?.result?.map(category => category?.children?.length > 0 ?
+                        <li
+                            key={category?._id}
+                            className='group'>
+
+                            <Link href='/products' className='flex items-center justify-between pl-4 py-1.5 text-xs xl:text-sm text-dark hover:bg-gray-100'> <span className='line-clamp-1'>{category?.title}</span> <IoIosArrowForward className='text-primary mr-4' /></Link>
                             {/* sub menu  */}
                             <ul className='absolute left-full bg-white w-full top-0 hidden group-hover:block py-2 h-full z-50'>
-                                <li>
-                                    <Link href='/' className='flex items-center justify-between pl-4 py-1.5 text-xs xl:text-sm text-dark hover:bg-gray-100'> <span className='line-clamp-1'>Mens & Fashion</span> <IoIosArrowForward className='text-primary mr-4' /></Link>
-                                </li>
-                                <li>
-                                    <Link href='/' className='flex items-center justify-between pl-4 py-1.5 text-xs xl:text-sm text-dark hover:bg-gray-100'> <span className='line-clamp-1'>Mens & Fashion</span> <IoIosArrowForward className='text-primary mr-4' /></Link>
-                                </li>
-                                <li>
-                                    <Link href='/' className='flex items-center justify-between pl-4 py-1.5 text-xs xl:text-sm text-dark hover:bg-gray-100'> <span className='line-clamp-1'>Mens & Fashion</span> <IoIosArrowForward className='text-primary mr-4' /></Link>
-                                </li>
-                                <li>
-                                    <Link href='/' className='flex items-center justify-between pl-4 py-1.5 text-xs xl:text-sm text-dark hover:bg-gray-100'> <span className='line-clamp-1'>Mens & Fashion</span> <IoIosArrowForward className='text-primary mr-4' /></Link>
-                                </li>
-                                {/* sub menu child */}
-                                <li className='group/sub'>
-                                    <Link href='/' className='flex items-center justify-between pl-4 py-1.5 text-xs xl:text-sm text-dark hover:bg-gray-100'> <span className='line-clamp-1'>Mens & Fashion</span> <IoIosArrowForward className='text-primary mr-4' /></Link>
-                                    <ul className='absolute left-full bg-white w-full top-0 hidden group-hover/sub:block py-2 h-full'>
-                                        <li>
-                                            <Link href='/' className='flex items-center justify-between pl-4 py-1.5 text-xs xl:text-sm text-dark hover:bg-gray-100'> <span className='line-clamp-1'>Mens & Fashion</span> <IoIosArrowForward className='text-primary mr-4' /></Link>
+                                {
+                                    category?.children?.map(subCat => subCat?.children?.length > 0 ?
+                                        <li key={subCat?._id} className='group/sub'>
+                                            <Link href='/' className='flex items-center justify-between pl-4 py-1.5 text-xs xl:text-sm text-dark hover:bg-gray-100'> <span className='line-clamp-1'>{subCat?.title}</span> <IoIosArrowForward className='text-primary mr-4' /></Link>
+                                            <ul className='absolute left-full bg-white w-full top-0 hidden group-hover/sub:block py-2 h-full'>
+                                                {
+                                                    subCat?.children?.map(subCatChild =>
+                                                        <li key={subCatChild?._id}>
+                                                            <Link href='/' className='flex items-center justify-between pl-4 py-1.5 text-xs xl:text-sm text-dark hover:bg-gray-100'> <span className='line-clamp-1'>{subCatChild?.title}</span></Link>
+                                                        </li>
+                                                    )
+                                                }
+                                            </ul>
+                                        </li> :
+                                        <li key={subCat?._id}>
+                                            <Link href='/' className='flex items-center justify-between pl-4 py-1.5 text-xs xl:text-sm text-dark hover:bg-gray-100'> <span className='line-clamp-1'>{subCat?.title}</span></Link>
                                         </li>
-                                        <li>
-                                            <Link href='/' className='flex items-center justify-between pl-4 py-1.5 text-xs xl:text-sm text-dark hover:bg-gray-100'> <span className='line-clamp-1'>Mens & Fashion</span> <IoIosArrowForward className='text-primary mr-4' /></Link>
-                                        </li>
-                                        <li>
-                                            <Link href='/' className='flex items-center justify-between pl-4 py-1.5 text-xs xl:text-sm text-dark hover:bg-gray-100'> <span className='line-clamp-1'>Mens & Fashion</span> <IoIosArrowForward className='text-primary mr-4' /></Link>
-                                        </li>
-                                        <li>
-                                            <Link href='/' className='flex items-center justify-between pl-4 py-1.5 text-xs xl:text-sm text-dark hover:bg-gray-100'> <span className='line-clamp-1'>Mens & Fashion</span> <IoIosArrowForward className='text-primary mr-4' /></Link>
-                                        </li>
-                                        <li>
-                                            <Link href='/' className='flex items-center justify-between pl-4 py-1.5 text-xs xl:text-sm text-dark hover:bg-gray-100'> <span className='line-clamp-1'>Mens & Fashion</span> <IoIosArrowForward className='text-primary mr-4' /></Link>
-                                        </li>
-                                    </ul>
-                                </li>
+                                    )
+                                }
+
                             </ul>
+                        </li> : <li key={category?._id}>
+                            <Link href='/' className='flex items-center justify-between pl-4 py-1.5 text-xs xl:text-sm text-dark hover:bg-gray-100'> <span className='line-clamp-1'>{category?.title}</span></Link>
                         </li>
                     )
                 }
@@ -55,3 +50,67 @@ const Categories = () => {
 };
 
 export default Categories;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// {
+//     Array(12).fill().map((_, idx) =>
+//         <li key={idx} className='group'>
+//             <Link href='/products' className='flex items-center justify-between pl-4 py-1.5 text-xs xl:text-sm text-dark hover:bg-gray-100'> <span className='line-clamp-1'>Womens & Girls Fashion</span> <IoIosArrowForward className='text-primary mr-4' /></Link>
+//             {/* sub menu  */}
+//             <ul className='absolute left-full bg-white w-full top-0 hidden group-hover:block py-2 h-full z-50'>
+//                 <li>
+//                     <Link href='/' className='flex items-center justify-between pl-4 py-1.5 text-xs xl:text-sm text-dark hover:bg-gray-100'> <span className='line-clamp-1'>Mens & Fashion</span> <IoIosArrowForward className='text-primary mr-4' /></Link>
+//                 </li>
+//                 <li>
+//                     <Link href='/' className='flex items-center justify-between pl-4 py-1.5 text-xs xl:text-sm text-dark hover:bg-gray-100'> <span className='line-clamp-1'>Mens & Fashion</span> <IoIosArrowForward className='text-primary mr-4' /></Link>
+//                 </li>
+//                 <li>
+//                     <Link href='/' className='flex items-center justify-between pl-4 py-1.5 text-xs xl:text-sm text-dark hover:bg-gray-100'> <span className='line-clamp-1'>Mens & Fashion</span> <IoIosArrowForward className='text-primary mr-4' /></Link>
+//                 </li>
+//                 <li>
+//                     <Link href='/' className='flex items-center justify-between pl-4 py-1.5 text-xs xl:text-sm text-dark hover:bg-gray-100'> <span className='line-clamp-1'>Mens & Fashion</span> <IoIosArrowForward className='text-primary mr-4' /></Link>
+//                 </li>
+//                 {/* sub menu child */}
+//                 <li className='group/sub'>
+//                     <Link href='/' className='flex items-center justify-between pl-4 py-1.5 text-xs xl:text-sm text-dark hover:bg-gray-100'> <span className='line-clamp-1'>Mens & Fashion</span> <IoIosArrowForward className='text-primary mr-4' /></Link>
+//                     <ul className='absolute left-full bg-white w-full top-0 hidden group-hover/sub:block py-2 h-full'>
+//                         <li>
+//                             <Link href='/' className='flex items-center justify-between pl-4 py-1.5 text-xs xl:text-sm text-dark hover:bg-gray-100'> <span className='line-clamp-1'>Mens & Fashion</span> <IoIosArrowForward className='text-primary mr-4' /></Link>
+//                         </li>
+//                         <li>
+//                             <Link href='/' className='flex items-center justify-between pl-4 py-1.5 text-xs xl:text-sm text-dark hover:bg-gray-100'> <span className='line-clamp-1'>Mens & Fashion</span> <IoIosArrowForward className='text-primary mr-4' /></Link>
+//                         </li>
+//                         <li>
+//                             <Link href='/' className='flex items-center justify-between pl-4 py-1.5 text-xs xl:text-sm text-dark hover:bg-gray-100'> <span className='line-clamp-1'>Mens & Fashion</span> <IoIosArrowForward className='text-primary mr-4' /></Link>
+//                         </li>
+//                         <li>
+//                             <Link href='/' className='flex items-center justify-between pl-4 py-1.5 text-xs xl:text-sm text-dark hover:bg-gray-100'> <span className='line-clamp-1'>Mens & Fashion</span> <IoIosArrowForward className='text-primary mr-4' /></Link>
+//                         </li>
+//                         <li>
+//                             <Link href='/' className='flex items-center justify-between pl-4 py-1.5 text-xs xl:text-sm text-dark hover:bg-gray-100'> <span className='line-clamp-1'>Mens & Fashion</span> <IoIosArrowForward className='text-primary mr-4' /></Link>
+//                         </li>
+//                     </ul>
+//                 </li>
+//             </ul>
+//         </li>
+//     )
+// }
