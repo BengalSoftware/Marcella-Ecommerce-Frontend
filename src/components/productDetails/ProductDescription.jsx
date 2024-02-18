a3a47e0d7b7e86026962f3a8a11d375e562e51b1
adnanhjoy <engr.adnanhossain@gmail.com>
2024-02-18 14:24:32 +0600

    add product description

--- src/components/productDetails/ProductDescription.jsx
+++ src/components/productDetails/ProductDescription.jsx
@@ -7,8 +7,10 @@ import toast from 'react-hot-toast';
 import WishlistButtonAndShare from './WishlistButtonAndShare';
 
 
-const ProductDescription = () => {
+const ProductDescription = ({ product }) => {
     const [productQty, setProductQty] = useState(1);
+    console.log(product)
+    const { name, categories, numReviews, totalRating, quantity, price, offerPrice, color, size } = product || {};
 
     const handleQtyIncrement = () => {
         const newQty = productQty + 1;
@@ -30,41 +32,45 @@ const ProductDescription = () => {
 
     return (
         <div>
-            <h1 className='text-lg lg:text-xl font-medium text-dark'>Nova Automatic Cordless Stainless Steel Electric Kettle 1.8 Liter Ae-1818</h1>
+            <h1 className='text-lg lg:text-xl font-medium text-dark'>{name}</h1>
             <div className='flex items-center justify-between'>
                 <div>
                     <p className='mt-2 text-sm lg:text-base'>Brand: No Brands</p>
-                    <p className='mt-2 text-sm lg:text-base'>Category: Home Appliance</p>
+                    {
+                        categories?.map(category =>
+                            <p key={category?._id} className='mt-2 text-sm lg:text-base'>Category: {category?.name}</p>
+                        )
+                    }
                 </div>
                 <WishlistButtonAndShare />
             </div>
             <div className='flex items-end justify-between mt-5'>
                 <span>
-                    <p className='line-through'>BDT 450</p>
-                    <h1 className='text-dark font-semibold text-2xl lg:text-3xl mt-3'>BDT 220</h1>
+                    <p className='line-through'>BDT {offerPrice && price}</p>
+                    <h1 className='text-dark font-semibold text-2xl lg:text-3xl mt-3'>BDT {offerPrice && offerPrice}</h1>
                 </span>
-                <p className='bg-primary text-white text-xs w-fit rounded-full px-2 py-0.5 flex items-center gap-1'><MdDiscount /> -BDT 230</p>
+                <p className='bg-primary text-white text-xs w-fit rounded-full px-2 py-0.5 flex items-center gap-1'><MdDiscount /> -BDT {price - offerPrice}</p>
             </div>
             <div className='flex items-center justify-between mt-3'>
-                <Rating rate={3.5} />
-                <p className='text-xs'>0 reviews</p>
+                <Rating rate={totalRating} />
+                <p className='text-xs'>{numReviews} reviews</p>
             </div>
             <div className='flex items-center gap-2 mt-4'>
                 <p>Color: </p>
-                <button className='h-5 w-5 rounded-full bg-red-500'></button>
-                <button className='h-5 w-5 rounded-full bg-black'></button>
-                <button className='h-5 w-5 rounded-full bg-blue-500'></button>
-                <button className='h-5 w-5 rounded-full bg-yellow-500'></button>
-                <button className='h-5 w-5 rounded-full bg-purple-500'></button>
+                {
+                    color?.map(col =>
+                        <button key={col?._id} className='h-5 w-5 rounded-full' style={{ backgroundColor: col?._id?.colorCode }}></button>
+                    )
+                }
             </div>
 
             <div className='flex items-center gap-2 mt-4'>
                 <p>Size: </p>
-                <button className='rounded-full text-sm text-dark'>S</button>
-                <button className='rounded-full text-sm text-dark'>M</button>
-                <button className='rounded-full text-sm text-dark'>XL</button>
-                <button className='rounded-full text-sm text-dark'>XXL</button>
-                <button className='rounded-full text-sm text-dark'>XXXL</button>
+                {
+                    size?.map(sz =>
+                        <button key={sz?._id} className='rounded-full text-sm text-dark'>{sz?.name}</button>
+                    )
+                }
             </div>
 
             <div className='mt-4 flex items-center gap-5'>
@@ -82,7 +88,7 @@ const ProductDescription = () => {
 
             <div className='flex items-center gap-x-20 mt-4'>
                 <button className='bg-primary text-white px-10 lg:px-16 py-2 lg:py-3 rounded-md hover:bg-dark ease-in-out duration-500'>Add to Cart</button>
-                <p className='text-sm font-light'>Stock: 10</p>
+                <p className='text-sm font-light'>Stock: {quantity}</p>
             </div>
         </div>
     );
