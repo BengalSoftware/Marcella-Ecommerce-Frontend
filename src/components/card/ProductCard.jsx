f3d5567443e484ee7decd5a48c3a8b1f995344fc
adnanhjoy <engr.adnanhossain@gmail.com>
2024-02-18 11:58:35 +0600

    add offerprice in product card

--- src/components/card/ProductCard.jsx
+++ src/components/card/ProductCard.jsx
@@ -4,10 +4,13 @@ import Link from 'next/link';
 
 const ProductCard = ({ imgs, product }) => {
     const { name, images, price, offerPrice } = product || {};
+    
     return (
         <Link href={`/product/Nova Automatic Cordless Stainless Steel Electric Kettle 1.8 Liter Ae-1818`}>
             <div className='bg-white rounded-lg cursor-pointer relative shadow'>
-                <p className='bg-g-primary absolute text-white top-0 left-0 rounded-tl-lg rounded-br-lg text-xs md:text-sm px-4 py-2 z-50'>Flat {price - offerPrice} TK off</p>
+                {
+                    price === offerPrice ? '' : <p className='bg-g-primary absolute text-white top-0 left-0 rounded-tl-lg rounded-br-lg text-xs md:text-sm px-4 py-2 z-50'>Flat {price - offerPrice} TK off</p>
+                }
                 <div className='overflow-hidden rounded-t-lg'>
                     <Image width={800} height={800} quality={100} className='rounded-t-lg hover:scale-110 ease-in-out duration-700 h-52' src={images?.[0] ? images?.[0] : imgs} alt="" />
                 </div>
