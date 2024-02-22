b6ec9fdea48ba09783f252284b2f0f1d63f71ac1
adnanhjoy <engr.adnanhossain@gmail.com>
2024-02-22 15:48:22 +0600

    add login

--- src/components/form/LoginForm.jsx
+++ src/components/form/LoginForm.jsx
@@ -1,24 +1,86 @@
+'use client'
+import { userLoggedIn } from '@/lib/authApi/authApi';
+import { Alert } from 'antd';
 import Link from 'next/link';
-import React from 'react';
+import { useRouter } from 'next/navigation';
+import React, { useState } from 'react';
+import toast from 'react-hot-toast';
 
 const LoginForm = () => {
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setIsError] = useState(null);
+    const router = useRouter();
+
+    let response;
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const emailPhone = e.target.emailPhone.value;
+        const password = e.target.password.value;
+
+        const getLoginInfo = {
+            email: emailPhone,
+            password
+        }
+
+        const userMutation = async () => {
+            if (getLoginInfo) {
+                try {
+                    setIsLoading(true);
+                    response = await userLoggedIn(getLoginInfo);
+                    if (response?.data) {
+                        router.push('/');
+                        toast.success('Login Successfull')
+                    } else {
+                        setIsError(true)
+                    }
+                } catch (error) {
+                    setIsError(true)
+                } finally {
+                    setIsLoading(false);
+                }
+            }
+        };
+
+        userMutation();
+
+        // e.target.reset()
+
+    }
+
+    const onClose = (e) => {
+        setIsError(false)
+    };
+
     return (
-        <form>
-            <input className="placeholder:text-dark placeholder:text-sm outline-none border-b w-full p-2 mt-10"
-                placeholder="Enter Email" type="email" />
-            <input className="placeholder:text-dark placeholder:text-sm outline-none border-b w-full p-2 mt-10"
-                placeholder="Password" type="password" />
+        <div className='pb-4'>
+            {
+                error &&
+                <Alert
+                    description="Authorizaton Failed Check phone or password"
+                    type="error"
+                    closable
+                    onClose={onClose}
+                    className='mt-4'
+                />
+            }
+            <form onSubmit={handleSubmit}>
+                <input className="placeholder:text-dark placeholder:text-sm outline-none border-b w-full p-2 mt-8"
+                    placeholder="Enter Email or Phone" name='emailPhone' type="email" />
+                <input className="placeholder:text-dark placeholder:text-sm outline-none border-b w-full p-2 mt-10"
+                    placeholder="Password" name='password' type="password" />
 
-            <button className="text-dark font-[300] text-sm pl-2 mt-5 hover:text-primary" type="button">Forgot
-                password ?</button>
+                <button className="text-dark font-[300] text-sm pl-2 mt-5 hover:text-primary" type="button">Forgot
+                    password ?</button>
 
-            <div className="mt-5 flex gap-5">
-                <button className="px-6 md:px-8 py-2 bg-primary text-white rounded-full hover:opacity-90">Login</button>
-                <Link href="/signup"
-                    className="px-6 md:px-8 py-2 hover:bg-secondary hover:text-primary rounded-full border border-dotted border-primary hover:opacity-90">Create
-                    Account</Link>
-            </div>
-        </form>
+                <div className="mt-5 flex gap-5">
+                    <button className="px-6 md:px-8 py-2 bg-primary text-white rounded-full hover:opacity-90">{isLoading ? 'Login..' : 'Login'}</button>
+                    <Link href="/signup"
+                        className="px-6 md:px-8 py-2 hover:bg-secondary hover:text-primary rounded-full border border-dotted border-primary hover:opacity-90">Create
+                        Account</Link>
+                </div>
+            </form>
+        </div>
     );
 };
 
--- src/lib/authApi/authApi.js
+++ src/lib/authApi/authApi.js
@@ -30,6 +30,38 @@ const sellerSignupMutation = async (data) => {
 };
 
 
+
+//user and seller login
+const userLoggedIn = async (data) => {
+    try {
+        const res = await fetch(`${baseUrl}/auth/login`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+            cache: 'force-cache'
+        });
+
+        if (!res.ok) {
+            throw new Error('Failed to fetch data');
+        }
+
+        const responseData = await res.json();
+
+        if (responseData) {
+            localStorage.setItem('sauth', JSON.stringify(responseData));
+        }
+
+        return responseData;
+    } catch (error) {
+        console.error('Error adding product:', error);
+        throw error;
+    }
+};
+
+
 export {
     sellerSignupMutation,
+    userLoggedIn
 }
