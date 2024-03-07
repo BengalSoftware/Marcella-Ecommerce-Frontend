4ed91912fbb2d52a49ec1b49852face1193cffba
adnanhjoy <engr.adnanhossain@gmail.com>
2024-03-07 14:35:47 +0600

    add top categorie

--- src/components/home/header/TopCategories.jsx
+++ src/components/home/header/TopCategories.jsx
@@ -1,18 +1,27 @@
 import React from 'react';
 import img from '../../../../public/assets/category.jpg'
 import Image from 'next/image';
+import Link from 'next/link';
+import { MdKeyboardDoubleArrowRight } from "react-icons/md";
+import { categoriesData } from '@/data/categoriesData';
 
 const TopCategories = () => {
     return (
-        <div className='bg-white shadow rounded-md md:grid grid-cols-8 gap-8 p-4 hidden mb-5'>
+        <div className='bg-white shadow rounded-md md:grid grid-cols-10 p-4 hidden mb-5'>
             {
-                Array(8).fill().map((_, idx) =>
-                    <div key={idx} className='flex flex-col items-center justify-center'>
-                        <Image quality={100} placeholder='blur' className='h-20 w-20 rounded-md' src={img} alt="category" />
-                        <p className='text-sm mt-2'>Gadget Express</p>
-                    </div>
+                categoriesData.map(category =>
+                    <Link key={category?.id} href={`/products?category=${category?.slug}`} className='flex flex-col items-center justify-center'>
+                        <Image quality={100} height={500} width={500} className='h-20 w-20 rounded-full' src={category?.image} alt={category?.name} />
+                        <p className='text-xs mt-2'>{category?.name}</p>
+                    </Link>
                 )
             }
+            <div className='flex flex-col items-center justify-center'>
+                <Link href='/products?category' className='bg-green-100 rounded-full h-20 w-20 flex flex-col items-center justify-center text-4xl text-primary'>
+                    <MdKeyboardDoubleArrowRight />
+                </Link>
+                <p className='text-xs mt-2'>All Categories</p>
+            </div>
         </div>
     );
 };
---
+++ src/data/categoriesData.js
@@ -0,0 +1,56 @@
+export const categoriesData = [
+    {
+        "id": 1,
+        "name": "Men's Fashion",
+        "image": "https://i.ibb.co/LC9fJvC/istockphoto-665032164-612x612.jpg",
+        "slug": 'mens-and-boys-fashion'
+    },
+    {
+        "id": 2,
+        "name": "Women's Fashion",
+        "image": "https://i.ibb.co/N6DXcNh/Clothes-Trends-Web-1.jpg",
+        "slug": 'womens-and-girls-fashion'
+    },
+    {
+        "id": 3,
+        "name": "Health & Beauty",
+        "image": "https://i.ibb.co/d0RPdn0/6479542e6bca96c4fa1be013-shutterstock-2193383875.jpg",
+        "slug": 'health-and-beauty'
+    },
+    {
+        "id": 4,
+        "name": "Home Appliance",
+        "image": "https://i.ibb.co/2YnLg4D/image.jpg"
+        , "slug": 'tv-and-home-applience'
+    },
+    {
+        "id": 5,
+        "name": "Groceries",
+        "image": "https://i.ibb.co/MVv9kny/7f1dcaea-grocery-delivery-driver-with-fresh-groceries.jpg",
+        "slug": 'groceries'
+    },
+    {
+        "id": 6,
+        "name": "Home & Lifestyle",
+        "image": "https://i.ibb.co/djhztrj/JoeGel02.jpg"
+        , "slug": 'home-and-lifestyle'
+    },
+    {
+        "id": 7,
+        "name": "Watch & Jewellery",
+        "image": "https://i.ibb.co/ZMtDP32/gold-jewelry-watch-necklaces-chain-rings-gift-box-picture-ai-generated-art-210643-1090.jpg",
+        "slug": 'watches-bags-jewellery'
+    },
+    {
+        "id": 8,
+        "name": "Sports & Outdoors",
+        "image": "https://i.ibb.co/L5tDNNC/outdoor-8473475-1280.webp",
+        "slug": 'sports-and-outdoors'
+    },
+    {
+        "id": 9,
+        "name": "Buy 2 Get 1",
+        "image": "https://i.ibb.co/Fh6Bvg2/buy-2-get-one-free-sale-banner-special-banner-with-editable-text-effect-535749-1517.jpg",
+        "slug": 'buy-2-get-one'
+    },
+]
