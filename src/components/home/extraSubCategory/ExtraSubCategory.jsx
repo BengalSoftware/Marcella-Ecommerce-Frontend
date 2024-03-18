151caa662f72367f451e7ca3d089be2738958ae0
adnanhjoy <engr.adnanhossain@gmail.com>
2024-03-18 10:47:28 +0600

    add extra sub category

--- src/components/home/extraSubCategory/ExtraSubCategory.jsx
+++ src/components/home/extraSubCategory/ExtraSubCategory.jsx
@@ -1,7 +1,6 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
-import img1 from '../../../../public/assets/category/free.gif'
 import { extraCatData } from '@/data/extraCatData';
 
 const ExtraSubCategory = () => {
@@ -9,7 +8,7 @@ const ExtraSubCategory = () => {
         <div className='bg-white shadow rounded-md md:grid grid-cols-7 p-4 hidden mb-5'>
             {
                 extraCatData.map(category =>
-                    <Link key={category?.id} href={`/products?category=${category?.slug}`} className='flex flex-col items-center justify-center'>
+                    <Link key={category?.id} href={category?.slug} className='flex flex-col items-center justify-center'>
                         <Image quality={100} height={500} width={500} className='h-20 w-20 rounded-md' src={category?.image} alt={category?.name} />
                         <p className='text-xs mt-2'>{category?.name}</p>
                     </Link>
--- src/data/extraCatData.js
+++ src/data/extraCatData.js
@@ -3,37 +3,37 @@ export const extraCatData = [
         "id": 1,
         "name": "Free Delivery",
         "image": "https://i.ibb.co/FY4TSHB/7b3222e4-db4b-4c3b-908d-6cb6b79cee58.png",
-        "slug": 'mens-and-boys-fashion'
+        "slug": '/products?category=mens-and-boys-fashion'
     },
     {
         "id": 2,
         "name": "Mart",
         "image": "https://i.ibb.co/kBsXspg/1440c427-d3fe-49f7-aa90-09c8eb0b84ea.png",
-        "slug": 'womens-and-girls-fashion'
+        "slug": '/products?category=groceries'
     },
     {
         "id": 3,
         "name": "Fashion",
         "image": "https://i.ibb.co/VTXqFvs/4f250051-aac7-46bd-9bea-c5b836dea844.png",
-        "slug": 'health-and-beauty'
+        "slug": '/products?category=mens-and-boys-fashion'
     },
     {
         "id": 4,
         "name": "Beauty",
         "image": "https://i.ibb.co/S6yk6rt/0ed92f69-d596-458b-bcd6-8d2bb69ef911.png"
-        , "slug": 'tv-and-home-applience'
+        , "slug": '/products?category=health-and-beauty'
     },
     {
         "id": 5,
         "name": "Home & Decor",
         "image": "https://i.ibb.co/qxcZD2Z/d85f0ced-6459-455a-8504-150655bf948c.png",
-        "slug": 'groceries'
+        "slug": '/products?category=tv-and-home-applience'
     },
     {
         "id": 6,
         "name": "Flash Sale",
         "image": "https://i.ibb.co/MfFHk0Y/e7b9ee8c-1fbf-4548-ab35-db6eeca830a6.png"
-        , "slug": 'home-and-lifestyle'
+        , "slug": '/campaigns?flashsale'
     },
     {
         "id": 7,
