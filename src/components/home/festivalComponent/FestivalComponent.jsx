7b5948d5d97bffce1d77dab89772ceee2d6677a5
adnanhjoy <engr.adnanhossain@gmail.com>
2024-03-18 12:24:45 +0600

    update flashsale component

--- src/components/home/festivalComponent/FestivalComponent.jsx
+++ src/components/home/festivalComponent/FestivalComponent.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import ProductCard from '../../card/ProductCard';
 import Image from 'next/image';
-import { getFlashSaleProduct } from '@/lib/flashSale/flashSaleApi';
+import { getFlashSaleOfferType, getFlashSaleProduct } from '@/lib/flashSale/flashSaleApi';
 import ProductSlider from '@/utility/productSlider/ProductSlider';
 import OfferCount from './OfferCount';
 import Link from 'next/link';
 
-const FestivalComponent = async ({ offerType, fesTitle, fesColor, fesImage, offerDate, endDate }) => {
-    const data = await getFlashSaleProduct(offerType);
-
+const FestivalComponent = async ({ offerType, fesTitle, fesColor, offerDate, endDate }) => {
+    const [flashType, flashProduct] = await Promise.all([
+        getFlashSaleOfferType(),
+        getFlashSaleProduct(offerType)
+    ])
+ 
     return (
         <div className='bg-white rounded-xl shadow mb-6'>
             <div className='flex items-center justify-between md:pr-5'>
@@ -20,12 +23,27 @@ const FestivalComponent = async ({ offerType, fesTitle, fesColor, fesImage, offe
                 </div>
                 <Link href={`/campaigns?flashsale=${offerType}`} className='border hidden md:block border-primary hover:bg-primary hover:text-white text-primary rounded-full px-4 py-1 font-medium ease-in-out duration-500'>Show All</Link>
             </div>
-            <Image quality={100} className='w-full h-fit' src={fesImage} alt="" />
+            {
+                flashType?.data?.map(img =>
+                    img?.name === offerType && (
+                        <Image
+                            height={500}
+                            width={1200}
+                            quality={100}
+                            className='w-full h-fit'
+                            src={img?.image}
+                            alt=""
+                        />
+                    )
+                )
+            }
+
+
 
             <div className={`px-2 md:px-5 py-5 rounded-b-xl`} style={{ backgroundColor: fesColor }}>
                 <ProductSlider>
                     {
-                        data?.result?.data?.map(product =>
+                        flashProduct?.result?.data?.map(product =>
                             <div key={product?._id} className='px-1 md:px-2'>
                                 <ProductCard
                                     product={product}
--- src/components/home/festivalComponent/index.jsx
+++ src/components/home/festivalComponent/index.jsx
@@ -1,4 +1,3 @@
-import ad3 from '../../../../public/assets/ad2.gif'
 import React from 'react';
 import FestivalComponent from './FestivalComponent';
 import { getFlashSaleOffer } from '@/lib/flashSale/flashSaleApi';
@@ -14,7 +13,6 @@ const AdsFestivalIndex = async () => {
                         <FestivalComponent
                             key={flashSale?._id}
                             fesColor={flashSale?.color}
-                            fesImage={flashSale?.image || ad3}
                             fesTitle={flashSale?.name}
                             offerDate={flashSale?.timeStamps}
                             endDate={flashSale?.endDate}
